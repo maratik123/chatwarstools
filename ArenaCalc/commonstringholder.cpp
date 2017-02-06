8ae3c64b354850a76c0f327354d96454eeb021ce
Marat Bukharov <maratik@yandex-team.ru>
2017-02-06 14:55:48 +0300

    Add keyboard shortcuts

--- ArenaCalc/commonstringholder.cpp
+++ ArenaCalc/commonstringholder.cpp
@@ -1,15 +1,13 @@
 #include "commonstringholder.h"
 
 CommonStringHolder::CommonStringHolder() :
-    stash(
-        {
+    stash({
             { StringID::ATTACK, QT_TR_NOOP("Attack %1") },
             { StringID::DEFENCE, QT_TR_NOOP("Defence %1") },
             { StringID::HEAD, QT_TR_NOOP("head") },
             { StringID::CHEST, QT_TR_NOOP("chest") },
             { StringID::LEGS, QT_TR_NOOP("legs") }
-        }
-        )
+          })
 {}
 
 const CommonStringHolder& CommonStringHolder::getInstance()
